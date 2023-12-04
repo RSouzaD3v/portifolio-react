2cfe43fa483f773d700ce1030e9cf17e503a479e
Rafael F. Souza <rafaelf.souzan02@gmail.com>
2023-12-03 21:08:48 -0300

    Translate no

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -3,7 +3,7 @@ import './styles.css'
 export const Intro = () => {
     return(
         <section className="intro">
-            <h1>Nice to meet you! <br /> 
+            <h1 translate='no'>Nice to meet you! <br /> 
             I'm <span className="myName">Rafael F Souza.</span></h1>
             <p>Desenvolvedor front-end, com grande noção no back-end,<br />
             brevemente poderei ser chamado de <span className='fullStackText'>Full-Stack.</span></p>
